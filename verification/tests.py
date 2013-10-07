29c2b90a3d35b273c873dd640b86800fa8d52e45
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-08 11:41:22 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -10,27 +10,52 @@ Each test is dict with
 
 TESTS = {
     "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+        {"input": [117, 17], "answer": 3},
+        {"input": [1, 2], "answer": 2},
+        {"input": [16, 15], "answer": 5},
+        {"input": [255, 1], "answer": 7},
+        {"input": [16, 16], "answer": 0},
+        {"input": [204, 157], "answer": 3},
+        {"input": [31, 51], "answer": 3},
+        {"input": [84, 198], "answer": 3},
+        {"input": [57, 39], "answer": 4},
+        {"input": [140, 160], "answer": 3},
+        {"input": [219, 185], "answer": 3},
+        {"input": [202, 138], "answer": 1},
+        {"input": [77, 149], "answer": 4},
+        {"input": [13, 200], "answer": 4},
+        {"input": [208, 251], "answer": 4},
+        {"input": [179, 186], "answer": 2},
+        {"input": [111, 59], "answer": 3},
+        {"input": [241, 114], "answer": 3},
+        {"input": [188, 141], "answer": 3},
+        {"input": [127, 20], "answer": 5},
+        {"input": [231, 131], "answer": 3},
+        {"input": [43, 21], "answer": 5},
+        {"input": [152, 157], "answer": 2},
+        {"input": [225, 193], "answer": 1},
+        {"input": [28, 33], "answer": 5}
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {"input": [77, 201], "answer": 2},
+        {"input": [1, 77], "answer": 3},
+        {"input": [67, 90], "answer": 3},
+        {"input": [27, 191], "answer": 3},
+        {"input": [71, 142], "answer": 4},
+        {"input": [196, 196], "answer": 0},
+        {"input": [203, 210], "answer": 3},
+        {"input": [136, 118], "answer": 7},
+        {"input": [231, 56], "answer": 7},
+        {"input": [106, 55], "answer": 5},
+        {"input": [172, 206], "answer": 3},
+        {"input": [132, 125], "answer": 6},
+        {"input": [235, 253], "answer": 3},
+        {"input": [140, 171], "answer": 4},
+        {"input": [35, 134], "answer": 4},
+        {"input": [184, 42], "answer": 3},
+        {"input": [39, 34], "answer": 2},
+        {"input": [237, 130], "answer": 6},
+        {"input": [104, 38], "answer": 4},
+        {"input": [139, 5], "answer": 4}
     ]
 }
