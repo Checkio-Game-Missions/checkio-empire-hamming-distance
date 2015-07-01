a652e04018229331dcf3af2686b80274955b8d85
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-07-01 13:38:51 +0300

    js update

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,25 +1,18 @@
-from checkio_referee import RefereeBase, validators, covercodes, representations
+from checkio_referee import RefereeBase, validators, covercodes, representations, ENV_NAME
 
 import settings_env
 from tests import TESTS
-
-Validator = validators.FloatEqualValidator
-
-Validator.PRECISION = 2
 
 
 class Referee(RefereeBase):
     TESTS = TESTS
     ENVIRONMENTS = settings_env.ENVIRONMENTS
     DEFAULT_FUNCTION_NAME = "hamming"
-    VALIDATOR = Validator
     ENV_COVERCODE = {
-        "python_2": covercodes.py_unwrap_args,
-        "python_3": covercodes.py_unwrap_args,
-        "javascript": covercodes.py_unwrap_args
+        ENV_NAME.PYTHON: covercodes.py_unwrap_args,
+        ENV_NAME.JS_NODE: covercodes.py_unwrap_args
     }
     CALLED_REPRESENTATIONS = {
-        "python_2": representations.unwrap_arg_representation,
-        "python_3": representations.unwrap_arg_representation,
-        "javascript": representations.unwrap_arg_representation,
+        ENV_NAME.PYTHON: representations.unwrap_arg_representation,
+        ENV_NAME.JS_NODE: representations.unwrap_arg_representation,
     }
